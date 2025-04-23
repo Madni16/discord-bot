c10e1cc579a846d1bb453ba3bb734e30562e6409
Madni <nico.barasinski@gmail.com>
2025-04-23 21:53:57 +0200

    Implement command cooldowns and event handling for interaction commands

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,8 +7,11 @@ const { token } = require('../../config.json');
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.commands = new Collection();
+// Create a cooldown collection for commands
+client.cooldowns = new Collection();
 
+// Collect commands from the commands directory and add them to the client's collection
+client.commands = new Collection();
 const foldersPath = path.join(__dirname, '..', 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
@@ -28,39 +31,19 @@ for (const folder of commandFolders) {
 	}
 }
 
-// When the client is ready, run this code (only once)
-// The distinction between `client: Client<boolean>` and `client: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable
+// Collect events from the events directory and listen for them
+const eventsPath = path.join(__dirname, '..', 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 // Log in to Discord with your client's token
 client.login(token);
-
-// Start listening to command events
-client.on(Events.InteractionCreate, async interaction => {
-	if(!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if(!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-		}
-		else {
-			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-		}
-	}
-
-	console.log(interaction);
-});
